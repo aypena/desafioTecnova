a9733e1130e44b10a60848646abc1fabfaea5126
yorxander <yorxander@gmail.com>
2025-08-04 19:01:25 -0400

    creacion de repositorio

---
+++ src/main/java/com/aypena/desafioTecnova/DesafioTecnovaApplication.java
@@ -0,0 +1,13 @@
+package com.aypena.desafioTecnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnovaApplication.class, args);
+	}
+
+}
